598e2bc2ce494206d99365a92d5950696d9ec3e3
amaltheon <r.lingham2@gmail.com>
2024-11-19 16:01:33 +0000

    added a few more linters for go and ts

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,6 +29,12 @@ return {
       lspconfig.html.setup({
         capabilities = capabilities
       })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
